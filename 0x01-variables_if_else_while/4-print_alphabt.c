b44e108949a2def58a6fbb1dfd9a2b89aa79671a
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-09 11:02:07 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - The entry point of the program
+ *
+ *   * Return: outps program result type, int
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+		char start;
+
+
+
+			for (start = 'a'; start <= 'z'; start++)
+
+					{
+
+								if (start == 'q' || start == 'e')
+
+											{
+
+															continue;
+
+																	}
+
+										putchar(start);
+
+											}
+
+				putchar('\n');
+
+					return (0);
+}
