083ec87f1a6afb55690c245aee1f45d2d8215b93
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-09 07:44:08 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - The entry point of the program
- *
- *   * Return: Outputs the result of the function type - 0
- *
- *    */
-
-
-
+ * main - The entry point of the program
+ * Return: Outputs the result of the function type - 0
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
