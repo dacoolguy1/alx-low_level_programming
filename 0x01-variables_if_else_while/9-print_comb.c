5aafb757c78a1b1d24b5aac421112e8451ec24ec
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-09 12:30:24 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - The entry point of the program
+  * Return: outputs result, int
+  */
+
+
+
+int main(void)
+
+{
+	unsigned int start;
+	for (start = 10; start < 20; start++)
+	{
+		putchar((start % 10) + '0');
+		if (start != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
