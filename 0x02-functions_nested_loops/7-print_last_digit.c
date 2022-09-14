38abc8319d285de95577692fc63d6d7ddcc0c4b2
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-14 07:18:22 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @num: Number
+ * Return: 1
+ */
+int print_last_digit(int num)
+{
+	num = num % 10;
+	if (num < 0)
+		num = num * -1;
+	_putchar(num + '0');
+	return (num);
+}
