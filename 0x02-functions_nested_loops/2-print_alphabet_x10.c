50c555a5174eda4297494da1c051519e0588d7c5
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-13 21:18:02 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - displays alphabets
+   * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char ah = 'a';
+	int count = 0;
+	
+	while (count < 10)
+	{
+		while (ah <= 'z')
+		{
+			_putchar(ah);
+			ah++;
+		}
+		_putchar('\n');
+		ah = 'a';
+		count = count + 1;
+	}
+}
