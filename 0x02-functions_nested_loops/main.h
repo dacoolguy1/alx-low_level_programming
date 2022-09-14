3026badc9b664383b474cdda91d4d54134606fe0
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-13 20:50:48 -0700

    0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,65 @@
+#ifndef _MAIN_H_
+
+#define MAIN_H
+
+/**
+ *
+ *  * File: main.h
+ *
+ *   * Description: Custom function prototype declarations
+ *
+ *    */
+
+
+
+/**
+ *
+ *  ** _putchar - writes a single character
+ *
+ *   * Return: 1
+ *
+ *    * @char: represents single English letter
+ *
+ *     */
+
+int _putchar(char c);
+
+
+
+/**
+ *
+ *  * print_alphabet: this displays lower case alphabets
+ *
+ *   * Return: void
+ *
+ *    */
+
+void print_alphabet(void);
+
+
+
+/**
+ *
+ *  * print_alphabet_x10: prints alphabets
+ *
+ *   * Return: void
+ *
+ *    */
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+int add(int,int);
+
+void print_to_98(int n);
+
+#endif
