c356688a790b03d917aabd34d8a76602b4274b40
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-14 11:48:00 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,84 +1,27 @@
-#include "main.h"
 #include <stdlib.h>
-
+#include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Baue
- * Return:void
- */
-
-
+ * jack_bauer - function that prints every minute of the day, 24 hour clock
+ *  h = hour, m = minutes
+   * / 10 allows second digit to rotate
+  * for loop breaks before passing 24:00
+   * Return: 24 hour clock line by line
+  */
 
 void jack_bauer(void)
 {
-	int h1;
-	int h2;
-	int m1;
-	int m2;
-	int a = 9;
+	int h, m;
 	
-	h2 = 0;
-
-	while (h2 <= 2)
-
+	for (h = 0; h < 24; h++)
 	{
-
-			if (h2 == 2)
-
-					{
-
-								/*Restrain to 23h, not 29*/
-
-								a = 3;
-
-									}
-
-				h1 = 0;
-
-					while (h1 <= a)
-
-							{
-
-										m2 = 0;
-
-												while (m2 <= 5)
-
-															{
-
-																			m1 = 0;
-
-																						while (m1 <= 9)
-
-																										{
-
-																															_putchar('0' + h2);
-
-																																			_putchar('0' + h1);
-
-																																							_putchar(':');
-
-																																											_putchar('0' + m2);
-
-																																															_putchar('0' + m1);
-
-																																																			_putchar('\n');
-
-																																																							m1++;
-
-																																																										}
-
-																									m2++;
-
-																											}
-
-														h1++;
-
-															}
-
-						h2++;
-
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
 	}
-
-
-
-}
