0748e826e013dbe7da82cd9c416c4f661a8c301b
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-15 13:39:06 -0700

    main file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - function to check for upper case alphabets
+ * @c: The character to be checked
+ * Return: return type is int
+ * File: main.h
+ * Description: Custom function prototype declarations
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int _putchar(char c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_num_buzz(void);
+void print_triangle(int size);
+void print_number(int n);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void print_to_98(int n);
+#endif
