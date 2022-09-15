72459d6b1f7c01666b5a72e51303d74330cbbeed
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-15 03:06:26 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - The function name
+ */
+
+void print_numbers(void)
+{
+	int i = '0';
+	
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
