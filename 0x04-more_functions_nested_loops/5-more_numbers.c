f8071ed5a1babc640a4142d39580da994dc8c41b
dacoolguy1 <emmanueltemiede@gmail.com>
2022-09-15 12:05:59 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * more_numbers - function name
+  */
+
+void more_numbers(void)
+{
+	int i = 1;
+	int j;
+	
+	while (i >= 0 && i <= 10)
+	{
+		j = 0;
+		while (j >= 0 && j <= 14)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
