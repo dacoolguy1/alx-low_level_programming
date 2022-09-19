59e748c23d95a8077a2096fd6a54da75c0e46931
ss <emmanueltemiede@gmail.com>
2022-09-19 11:25:51 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function name
+ * @a: first param
+ * @b: second param
+ */
+void swap_int(int *a, int *b)
+{
+	int temp1;
+
+	temp1 = *a;
+
+	*a = *b;
+	*b = temp1;
+}
