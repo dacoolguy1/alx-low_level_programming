84e166400502aacad426cb8e88d9d50f1d490e50
ss <emmanueltemiede@gmail.com>
2022-09-19 11:33:10 -0700

    function to find the length of strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function name
+ * @s: parameter
+ * Return: lenght of string of type int
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (s[len] != 0)
+		len++;
+	return (len);
+}
